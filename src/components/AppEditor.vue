bd94f0299a8b255ce88c5bfd6f9f0b407cc84236
Mihai Pavelescu <media.expres@gmail.com>
2023-07-16 14:35:11 +0300

    starting adding image to canvas

--- src/components/AppEditor.vue
+++ src/components/AppEditor.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="my-8">
-      <canvas width="448" height="448"></canvas>
+      <canvas width="448" height="448" ref="canvasEl"></canvas>
       <div class="text-white text-xl mt-4">
         <div class="flex justify-center gap-4">
           <button type="button" class="py-4 w-full"
