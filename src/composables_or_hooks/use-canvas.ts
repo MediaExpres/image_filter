805a255711e571322821bc2bdac344df88dcef75
Mihai Pavelescu <media.expres@gmail.com>
2023-07-17 16:22:21 +0300

    finishing to write use-canvas file code

--- src/composables_or_hooks/use-canvas.ts
+++ src/composables_or_hooks/use-canvas.ts
@@ -4,9 +4,26 @@ export default function useCanvas() {
 const canvasEl = ref<HTMLCanvasElement | null>(null);
 let canvasCtx: CanvasRenderingContext2D | null = null;
 
-function loadImage() {
+const imgEl = new Image();
+
+function loadImage(url: string) {
     if(!canvasEl.value) return;
 
     canvasCtx = canvasEl.value.getContext("2d");
+
+    imgEl.addEventListener("load", drawOriginalImage);
+
+    imgEl.src = url;
+
+}
+function drawOriginalImage () {
+    if(!canvasCtx || !canvasEl.value) return;
+
+    canvasCtx.drawImage(imgEl, 0, 0);
+}
+
+return {
+    canvasEl,
+    loadImage,
 }
 }
