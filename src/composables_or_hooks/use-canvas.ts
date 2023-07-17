d4dd730261897b88c1ef406a337c0e91cd9b08c1
Mihai Pavelescu <media.expres@gmail.com>
2023-07-17 16:15:00 +0300

    writing use-canvast.ts code

--- src/composables_or_hooks/use-canvas.ts
+++ src/composables_or_hooks/use-canvas.ts
@@ -1,5 +1,12 @@
 import { ref } from "vue";
 
 export default function useCanvas() {
-const canvasEl = ref(null);
+const canvasEl = ref<HTMLCanvasElement | null>(null);
+let canvasCtx: CanvasRenderingContext2D | null = null;
+
+function loadImage() {
+    if(!canvasEl.value) return;
+
+    canvasCtx = canvasEl.value.getContext("2d");
+}
 }
